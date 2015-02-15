4b559d014e75bf5618e8c847b1bae017d865ca31
hakrsekar <sekar.srilalabs@gmail.com>
2015-02-15 19:03:13 +0530

    Chapter 3

---
+++ Dromey/Chapter3/3.1_Square_Root.c
@@ -0,0 +1,29 @@
+/* Chapter 3.1 Find Square Root of a Number
+	Sekar Che, Feb 2015 */
+	
+#include<stdio.h>
+
+int main()
+{
+	float m;
+	float n;
+	float num;
+	n = 0.0001;   
+	
+	printf ("Enter a Number : ");
+	scanf ("%f",&num);
+ 
+	for (m = 0; m < num; m = m + n) {
+		
+		if((m * m) > num) {
+			m = m - n;          
+			break;      
+		}
+	}
+	
+	printf("%.5f",m);
+	return 0;
+}
+
+
+
