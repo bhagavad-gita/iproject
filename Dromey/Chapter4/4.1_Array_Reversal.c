74d84b93216507fdc3a5269fb3d9d8d6f9a4ae9d
hakrsekar <sekar.srilalabs@gmail.com>
2015-02-19 18:22:06 +0530

    Chapter4

--- Dromey/Chapter4/4.1_Array_Reversal.c
+++ Dromey/Chapter4/4.1_Array_Reversal.c
@@ -5,24 +5,27 @@
 
 int main()
 {
+	int i;
     int n;
     int a[100];
-    int i;
-    int temp;
+
+
     
-    printf("Total no of Elements : ");
-    scanf("%d", &n);
+    printf ("Total no of Elements : ");
+    scanf ("%d", &n);
     
     printf ("Enter the Elements:\n");
-    for(i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
           scanf("%d", &a[i]);
     } 
     
     printf ("Array Reverasl:\n");
-    for(i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
     	a[i] = n - i;
           printf (" %d",a[i]);
     }
     
 	return 0;
 }
+
+
